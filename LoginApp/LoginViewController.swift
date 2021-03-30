d7d89663589db6eacdcd17b2c7410d60f3462c45
David <ber.dev@yandex.ru>
2021-03-30 16:43:09 +0300

    logic: welcome + userName

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -7,17 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
     
     @IBOutlet weak var logInButton: UIButton!
+    
     @IBOutlet weak var forgotNameButton: UIButton!
     @IBOutlet weak var forgotPasswordButton: UIButton!
+    
+    @IBOutlet weak var userNameTF: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = userNameTF.text
+    }
 }
+
 
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    @IBOutlet weak var logOutButton: UIButton!
+    
+    var userName: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        welcomeLabel.text = ("Welcome, " + userName)
+    }
+    @IBAction func logOutAction() {
+        dismiss(animated: true)
     }
     
 
