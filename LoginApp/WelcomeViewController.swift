b5ea818465b75bc5622e7b6331fc7ed0dda946eb
David <ber.dev@yandex.ru>
2021-03-30 14:46:18 +0300

    Create WelcomeViewController

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Дэвид Бердников on 30.03.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
