74158a7327061c126f806653230c55c3e84bdecf
David <ber.dev@yandex.ru>
2021-04-04 18:21:28 +0300

    I DID IT, GOD!

--- LoginApp/Models/User.swift
+++ LoginApp/Models/User.swift
@@ -14,8 +14,6 @@ struct User {
 struct Person {
     let name = "David"
     let secondName = "Berdnikov"
-    let job = "Finance"
-    let hobby = "SkiSport"
     let question = "Hi, this is question by Google. What is the next number after 10, 9, 60, 90, 70, 66?"
     let answer = "one-googol"
     let info = "Call me David \n Live in Moscow \n Study in swiftbook \n beginner in swift"
